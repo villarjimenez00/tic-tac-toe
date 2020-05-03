b0468c215a588c4bdc19d7a367f5328dc2df2144
Alejandro Villar Jimenez <villarjimenez00@gmail.com>
2020-05-03 18:21:23 +0200

    add game board view

---
+++ src/general.ts
@@ -0,0 +1,7 @@
+import { css } from 'lit-element'
+
+export const general = css`
+  * {
+    box-sizing: border-box;
+  }
+`
--- src/tic-tac-toe.ts
+++ src/tic-tac-toe.ts
@@ -1,9 +1,49 @@
-import { customElement, LitElement } from 'lit-element'
+import { customElement, LitElement, css } from 'lit-element'
 import { html } from 'lit-html'
+import { general } from './general'
 
 @customElement('tic-tac-toe')
 export class TicTacToe extends LitElement {
+  static get styles() {
+    return [
+      general,
+      css`
+    .game{
+      display:flex;
+      justify-content:center;
+      aling
+    }
+      .board {
+        display: grid;
+        max-width:600px;
+        grid-template-columns: repeat(3, 280px);
+        grid-template-rows: repeat(3, 280px);
+        grid-gap: 20px;
+      }
+      .cell {
+        background-color: tomato;
+      }
+    `
+    ]
+  }
+
   render() {
-    return html`<h1>Hello world</h1>`
+    return html`
+      <main class="game">
+        <section class="board">
+          <div class="cell"></div>
+          <div class="cell"></div>
+          <div class="cell"></div>
+
+          <div class="cell"></div>
+          <div class="cell"></div>
+          <div class="cell"></div>
+
+          <div class="cell"></div>
+          <div class="cell"></div>
+          <div class="cell"></div>
+        </section>
+      </main>
+    `
   }
 }
